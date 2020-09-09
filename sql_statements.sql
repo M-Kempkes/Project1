297f78a9cb3f17196e9964d4928d92486c5fb717
M-Kempkes <2088979@talnet.nl>
2020-09-09 12:19:53 +0200

    SQL statements

---
+++ sql_statements.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE project1;
+CREATE TABLE account(
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255)NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE(email)
+); 
+CREATE TABLE persoon(
+    id int NOT NULL AUTO_INCREMENT,
+    voornaam varchar(255),
+    achternaam varchar(255),
+    username varchar(255),
+    accountid int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (accountid) REFERENCES account(id),
+    UNIQUE(username)
+);
