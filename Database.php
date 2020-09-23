03305232d26a8015e5fa68e7be6645a2c51cb98c
M-Kempkes <2088979@talnet.nl>
2020-09-23 13:30:02 +0200

    changed options location

--- Database.php
+++ Database.php
@@ -9,14 +9,13 @@ define ('DB_CHARSET', 'utf8mb4');
 class Database{
     protected $DB;
     protected $stmt;
-    protected $options;
+    protected $options= [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
     
     function __construct(){
-        $this->options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
         try {
             $this->DB = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET, DB_USER, DB_PASS, $this->options);
         } catch (\PDOException $e) {
